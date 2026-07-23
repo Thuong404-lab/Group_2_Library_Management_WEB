4313f894b0b480c0da1951584cfe65370526e092
Thuong404-lab <nhoc2323@gmail.com>
2026-07-23 19:45:43 +0700

    feat: add book management interface and remove unused localization tables

--- src/main/resources/db/migration/V20260723191000__drop_unused_translation_tables.sql
+++ src/main/resources/db/migration/V20260723191000__drop_unused_translation_tables.sql
@@ -2,19 +2,29 @@
  * Drop foreign keys referencing LocalizationLanguages from active tables 
  * so we can drop the LocalizationLanguages table itself.
  */
-ALTER TABLE dbo.Member_Accounts DROP CONSTRAINT FK_Member_Accounts_language;
-ALTER TABLE dbo.Notifications DROP CONSTRAINT FK_Notifications_language;
-ALTER TABLE dbo.MembershipTierTranslations DROP CONSTRAINT FK_MembershipTierTranslations_language;
+ALTER TABLE dbo.Member_Accounts
+DROP CONSTRAINT FK_Member_Accounts_language;
+
+ALTER TABLE dbo.Notifications
+DROP CONSTRAINT FK_Notifications_language;
+
+ALTER TABLE dbo.MembershipTierTranslations
+DROP CONSTRAINT FK_MembershipTierTranslations_language;
 
 /* 
- * Drop unused translation tables that have no Java Entities
- * and are not used by the application.
- */
+* Drop unused translation tables that have no Java Entities
+* and are not used by the application.
+*/
 DROP TABLE IF EXISTS dbo.CategoryTranslations;
+
 DROP TABLE IF EXISTS dbo.GenreTranslations;
+
 DROP TABLE IF EXISTS dbo.BookTranslations;
+
 DROP TABLE IF EXISTS dbo.ShelfTranslations;
+
 DROP TABLE IF EXISTS dbo.NotificationTranslations;
+
 DROP TABLE IF EXISTS dbo.SystemSettingTranslations;
 
 /* Finally drop LocalizationLanguages */
